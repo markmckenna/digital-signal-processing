415ade52ac9c63c2c7b3cc4e5495063231aa4fd6
Mark McKenna <mark.mckenna@redspace.com>
2024-10-16 13:19:50 -0300

    Add matplotlib; add sample code

---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+t = np.arange(100) # array containing numbers from [0..99] (think of them as seconds)
+s = np.sin(0.15*2*np.pi*t) # array containing 100 samples of a sine wave at 0.15hz, one per second
+S = np.fft.fft(s) # fourier space transform of s: 100 elements showing 
