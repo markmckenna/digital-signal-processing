c8bc0545c5c52a93dde29451e7ca64ede1604733
Mark McKenna <mark.mckenna@redspace.com>
2024-10-16 13:27:03 -0300

    update code to graph simple stuff

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import numpy as np
 
 t = np.arange(100) # array containing numbers from [0..99] (think of them as seconds)
 s = np.sin(0.15*2*np.pi*t) # array containing 100 samples of a sine wave at 0.15hz, one per second
-S = np.fft.fft(s) # fourier space transform of s: 100 elements showing 
+S = np.fft.fft(s) # frequency domain (Fourier space) representation of s
 
 import matplotlib.pyplot as plt
+S_mag = np.abs(S) # array of magnitudes of Fourier signals
+S_phase = np.angle(S) # array of phases of Fourier signals
+plt.plot(t, S_mag, '.-')
+plt.plot(t, S_phase, '.-')
+
+plt.show()
