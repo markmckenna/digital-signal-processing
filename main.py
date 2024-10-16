c76c5fd04aca5d6e010f65dec21211a0798daff7
Mark McKenna <mark.mckenna@redspace.com>
2024-10-16 14:32:51 -0300

    Generate spectrogram of simulated audio signal

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import numpy as np
-
-t = np.arange(100) # array containing numbers from [0..99] (think of them as seconds)
-s = np.sin(0.15*2*np.pi*t) # array containing 100 samples of a sine wave at 0.15hz, one per second
-S = np.fft.fft(s) # frequency domain (Fourier space) representation of s
-
 import matplotlib.pyplot as plt
-S_mag = np.abs(S) # array of magnitudes of Fourier signals
-S_phase = np.angle(S) # array of phases of Fourier signals
-plt.plot(t, S_mag, '.-')
-plt.plot(t, S_phase, '.-')
+
+sample_rate = 44100 # 44.1Khz sample rate
+sample_window = 1 # 1 second worth of samples
+
+# generate time markers spread evenly across the time domain
+t = np.arange(sample_rate*sample_window)/sample_rate 
+
+# Generate the tone itself, one for each time marker
+f = 10e3 # the frequency of the tone we are generating (10Khz; "kind of high")
+x = np.sin(2*np.pi*f*t)
+
+# Set up to do an FFT with a given slice size against the above signal
+slice_size = 1024 # how many samples each FFT instance sees
+num_slices = sample_rate//slice_size # how many slices we take
+
+# Allocate space for the spectrogram data
+spectrogram = np.zeros((num_slices, slice_size)) # build the spectrogram as a 2d array (starting with zeroes)
+
+# Generate the spectrogram--each row gets the FFT for one slice.
+for i in range(num_slices):
+    spectrogram[i,:] = 10*np.log10(np.abs(np.fft.fftshift(np.fft.fft(x[i*slice_size:(i+1)*slice_size])))**2)
+
+# Render the spectrogram with width scaled in Khz
+f_unit = 1e3 # freq shown in Khz
+plt.imshow(spectrogram, aspect='auto', extent = [ sample_rate/-2/f_unit, sample_rate/2/f_unit, len(x)/sample_rate, 0])
+plt.xlabel("Frequency [Khz]")
+plt.ylabel("Time [s]")
 
 plt.show()
